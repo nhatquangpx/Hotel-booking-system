8232d646a9d3a9a8f8bc403e49e7c61197645169
nhatquangpx <doannhatquang0@gmail.com>
2025-03-23 23:50:04 +0700

    Categories.scss

--- client/src/components/Categorises.jsx
+++ client/src/components/Categorises.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import { categories } from '../data'
+import "../styles/Categorises.scss"
 const Categorises = () => {
   return (
     <div className='catergories'>
@@ -7,7 +7,18 @@ const Categorises = () => {
         <p>Khám phá những căn phòng được thiết kế sang trọng, đầy đủ tiện nghi, mang đến cảm giác thoải mái như ở nhà. Chúng tôi cam kết cung cấp dịch vụ chất lượng với đội ngũ nhân viên tận tâm, sẵn sàng hỗ trợ 24/7. Hãy chọn cho mình một không gian lý tưởng và tận hưởng kỳ nghỉ tuyệt vời cùng những tiện ích đẳng cấp.</p>
     
     <div className='catergories_list'>
-
+        {categories?.slice(1, 7).map((category, index)=>{
+          <Link to=''>
+            <div className ='category' key={index}>
+              <img src="category.img" alt='category.label'/>
+              <div className="overlay"></div>
+              <div className="category_text">
+                <div className="category_text_icon">{category.icon}</div>
+                <p>{category.label}</p>
+              </div>
+            </div>
+          </Link>
+        })}
     </div>
     </div>
   )
