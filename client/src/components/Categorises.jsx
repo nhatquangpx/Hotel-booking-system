1bb352d4332c4bc8a76495b6632fb193a12c8177
nhatquangpx <doannhatquang0@gmail.com>
2025-03-23 23:40:26 +0700

    Room data

--- client/src/components/Categorises.jsx
+++ client/src/components/Categorises.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 
-const RoomList = () => {
+const Categorises = () => {
   return (
-    <div className='room'>
+    <div className='catergories'>
         <h1>Chọn Phòng Bạn Cảm Thấy Yêu Thích</h1>
         <p>Khám phá những căn phòng được thiết kế sang trọng, đầy đủ tiện nghi, mang đến cảm giác thoải mái như ở nhà. Chúng tôi cam kết cung cấp dịch vụ chất lượng với đội ngũ nhân viên tận tâm, sẵn sàng hỗ trợ 24/7. Hãy chọn cho mình một không gian lý tưởng và tận hưởng kỳ nghỉ tuyệt vời cùng những tiện ích đẳng cấp.</p>
     
-    <div className='room_list'>
-      {roomlist}
+    <div className='catergories_list'>
 
     </div>
     </div>
   )
 }
 
-export default RoomList
+export default Categorises
