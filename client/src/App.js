09dd84939cf249fcb493499edda652ab2d389aa8
nhatquangpx <doannhatquang0@gmail.com>
2025-03-26 00:27:15 +0700

    Update Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/AdminDashboard";
+import StaffDashboard from "./pages/StaffDashboard";
 import PrivateRoute from "./components/PrivateRoute";
-import './App.css';
+import "./App.css";
 
 function App() {
   const user = useSelector((state) => state.auth?.user || null);
@@ -13,14 +14,30 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
 
-        <Route path="/dashboard" element={user?.role === "admin" ? <Navigate to="/admin" /> : <HomePage />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            user ? (
+              user.role === "admin" ? <Navigate to="/admin" /> 
+              : user.role === "staff" ? <Navigate to="/staff" /> 
+              : <Navigate to="/" />
+            ) : (
+              <Navigate to="/login" />
+            )
+          } 
+        />
 
         <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
           <Route path="/admin" element={<AdminDashboard />} />
+        </Route>
+
+        <Route element={<PrivateRoute allowedRoles={["staff"]} />}>
+          <Route path="/staff" element={<StaffDashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
